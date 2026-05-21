fd6ebea81d802d78daf9638102d68a5141125f96
Kaninchen221 <pawel8krolik@gmail.com>
2026-05-22 01:00:05 +0200

    Update the source code

--- MASMOR/main.asm
+++ MASMOR/main.asm
@@ -1,3 +1,9 @@
+COMMENT *
+Program purpose: Learning
+Author: Pawel Krolik
+*
+
+STD_OUTPUT_HANDLE_VALUE = -11
 
 ; Enables instructions 80386 and 80387 processors instructions
 .386
@@ -20,15 +26,30 @@ ENDM
 
 .data
 
-	std_output_handle DWORD ?
+	std_output_handle dword ?
 
 	console_title byte "MASMOR", 0
 
 	hello_message byte "MASMOR Start!", 13, 10
-	hello_message_size DWORD $ - hello_message
+	hello_message_size dword $ - hello_message
 
-	test_message byte "TEST_MESSAGE", 13, 10
-	test_message_size DWORD $ - test_message
+	test_message byte "TEST_MESSAGE "
+		byte "and there is another line", 13, 10
+
+	; It should be always divided by the size of an one element, single char is one byte so we divide the size by 1
+	; $ is the current location counter, returns the offset to current program statement
+	test_message_size dword ($ - test_message) / 1
+
+	; Allocate 20 bytes ~ uninitialized
+	dup_test byte 20 dup(?)
+
+	main_loop_counter dword 0
+
+	; Binary coded decimal with valid initialization value
+	tbyte_test TBYTE 800000000000001234h
+
+	; Floats (real4, real8, real10)
+	real_test real4 -1.2
 
 .code
 
@@ -42,7 +63,7 @@ MR_test endp
 main proc
 
 	; STD OUTPUT HANDLE
-	invoke GetStdHandle, -11 ; -11 is STD_OUTPUT_HANDLE
+	invoke GetStdHandle, STD_OUTPUT_HANDLE_VALUE ; -11 is STD_OUTPUT_HANDLE
 	mov std_output_handle, EAX
 
 	MR_WriteConsole hello_message, hello_message_size
@@ -54,7 +75,14 @@ main proc
 		;MR_WriteConsole test_message, test_message_size
 		; Invoke a function
 		invoke MR_test
-		jmp main_loop
+		inc main_loop_counter
+
+		mov eax, 0
+		inc eax
+
+		.if main_loop_counter < 10
+			jmp main_loop
+		.endif
 
 	invoke ExitProcess, EAX
 
